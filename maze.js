bead6f7287c6b3d7f0eb53e0effdd145a43b59ca
jo-gianna <jogianna10@gmail.com>
2018-12-07 13:35:45 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,11 +1,18 @@
 window.onload = function(){
-	let boundary1 = document.getElementById("boundary1");
-	boundary1.onmouseover = red;
+	//let boundary1 = document.getElementById("boundary1");
+	//boundary1.onmouseover = red;
+
+	let boundary = document.querySelectorAll("#maze .boundary");
+	for (let i=0; i<boundary.length; i++){
+		boundary[i].onmouseover = red;
+	}
 
 	function red(){
-		
+		for (let i=0; i<boundary.length; i++){
+			boundary[i].classList.add("youlose");
+		}
 		//boundary1.setAttribute("class", "boundary youlose");
-		boundary1.classList.add("youlose");
+		//boundary1.classList.add("youlose");
 	}
 	
 }
