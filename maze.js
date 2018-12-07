5a8cf806204ecf9f248607c1373b9e6bda106f4e
jo-gianna <jogianna10@gmail.com>
2018-12-07 13:44:29 -0500

    Exercise 3

--- maze.js
+++ maze.js
@@ -3,16 +3,28 @@ window.onload = function(){
 	//boundary1.onmouseover = red;
 
 	let boundary = document.querySelectorAll("#maze .boundary");
+	let hitBoundary = false;
 	for (let i=0; i<boundary.length; i++){
 		boundary[i].onmouseover = red;
 	}
 
+	let end = document.getElementById("end");
+
+	end.onmouseover = popup;
+
 	function red(){
 		for (let i=0; i<boundary.length; i++){
 			boundary[i].classList.add("youlose");
+			hitBoundary = true;
 		}
 		//boundary1.setAttribute("class", "boundary youlose");
 		//boundary1.classList.add("youlose");
 	}
-	
+	function popup(){
+		if(hitBoundary==true){
+			alert("You Lose!");
+		}else{
+			alert("You Win!");
+		}
+	}
 }
